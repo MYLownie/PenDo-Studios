0906359d8b2459e65c9f686db9a3b6e24a1ee8c4
MYLownie <66714911+MYLownie@users.noreply.github.com>
2020-09-16 16:15:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+//LIGHT MODE || DARK MODE
+//toggles between two diff stylesheets when sun svg is clicked
+
+//grabs the sun svg and the stylesheet link
+const sunBtn = document.querySelector("#sun");
+
+const theme = document.querySelector("#theme-link");
+
+
+//if the sun is clicked and the the stylesheet link's href is already light theme, set to dark, else set to light
+sunBtn.addEventListener("click", function() {
+    if (theme.getAttribute("href") == "light-theme.css") {
+        theme.href = "dark-theme.css";
+    } else {
+        theme.href = "light-theme.css";
+    }
+});
+
+
+//Hidden Burger Menu appear and disappear function
+const menuAppear = () => {
+
+    //selects the burger svg and the hidden menu element
+    const burger = document.getElementById('burger');
+    const hiddenMenu = document.getElementsByClassName('hidden-menu');
+
+    //uses a loop because the hidden menu is an array
+    //toggles a new "burger-active" class to the buttons when burger is clicked
+    burger.addEventListener('click',()=>{
+        for (a = 0; a < hiddenMenu.length; a++) {
+            hiddenMenu[a].classList.toggle('burger-active');
+        }
+    });
+}
+//function must be called
+menuAppear();
+
+
+//two different values, top for Safari, bottom for others (chrome etc.)
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
